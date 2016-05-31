e6419cd6747862d053ee8bea23c7fdbc735fcacf
sc1991327 <sc1991327@gmail.com>
2016-05-31 14:03:18 +0800

    solve multi-computer test problem. solve unity crush problem.

--- testNetwork/Assets/NetManagerUDP.cs
+++ testNetwork/Assets/NetManagerUDP.cs
@@ -11,7 +11,7 @@ using System.Threading;
 public class NetManagerUDP : MonoBehaviour {
 
     public static string myIP = "127.0.0.1";
-    public static string targetIP = "127.0.0.1";
+    public static string serverIP = "10.32.93.177";
     public static int sendPort = 8885;
     public static int recvPort = 8886;
 
@@ -19,6 +19,8 @@ public class NetManagerUDP : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        myIP = GetLocalIPAddress();
 
         clientUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         clientUDP.Bind(new IPEndPoint(IPAddress.Parse(myIP), recvPort));
@@ -37,10 +39,16 @@ public class NetManagerUDP : MonoBehaviour {
 
 	}
 
+    void OnDisable()
+    {
+        clientUDP.Close();
+    }
+
     static void sendMsg(string message)
     {
-        EndPoint point = new IPEndPoint(IPAddress.Parse(targetIP), sendPort);
+        EndPoint point = new IPEndPoint(IPAddress.Parse(serverIP), sendPort);
         clientUDP.SendTo(Encoding.UTF8.GetBytes(message), point);
+        Debug.Log("Send: " + point.ToString() + " " + message);
     }
 
     static void ReciveMsg()
@@ -51,7 +59,20 @@ public class NetManagerUDP : MonoBehaviour {
             byte[] buffer = new byte[1024];
             int length = clientUDP.ReceiveFrom(buffer, ref point);
             string message = Encoding.UTF8.GetString(buffer, 0, length);
-            Debug.Log(point.ToString() + message);
+            Debug.Log("Recv: " + point.ToString() + " " + message);
         }
+    }
+
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("Local IP Address Not Found!");
     }
 }
