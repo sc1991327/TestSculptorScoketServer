59061fa38e8dcccbb1ac04cf80784fbb327ec68c
sc1991327 <sc1991327@gmail.com>
2016-05-31 10:29:52 +0800

    implement test TCP & UDP Socket between unity and c# server

---
+++ testNetwork/Assets/NetManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System;
+
+public class NetManager : MonoBehaviour {
+
+    public string myIP = "10.32.93.177";
+    public int myProt = 8885;
+
+    private Socket clientSocket;
+    private Thread thread;
+    private byte[] data = new byte[1024];  
+    private string message = "";
+
+    // Use this for initialization
+    void Start () {
+        ConnectToServer();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            string value = "Hello World";
+            SendMessage(value);
+        }
+    }
+
+    void ConnectToServer()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientSocket.Connect(new IPEndPoint(IPAddress.Parse(myIP), myProt));
+        thread = new Thread(ReceiveMessage);
+        thread.Start();
+    }
+
+    void ReceiveMessage()
+    {
+        while (true)
+        {
+            if (clientSocket.Connected == false)
+            {
+                break;
+            }
+            int length = clientSocket.Receive(data);
+            message = Encoding.UTF8.GetString(data, 0, length);
+            print(message);
+        }
+    }
+
+    void SendMessage(string message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        clientSocket.Send(data);
+    }
+
+    void OnDestroy()
+    {
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+    }
+}
---
+++ testNetwork/Assets/NetManagerUDP.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class NetManagerUDP : MonoBehaviour {
+
+    public static string myIP = "127.0.0.1";
+    public static string targetIP = "127.0.0.1";
+    public static int sendPort = 8885;
+    public static int recvPort = 8886;
+
+    private static Socket clientUDP;
+
+	// Use this for initialization
+	void Start () {
+
+        clientUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        clientUDP.Bind(new IPEndPoint(IPAddress.Parse(myIP), recvPort));
+        Thread t = new Thread(ReciveMsg);
+        t.Start();
+        Debug.Log("Client Start...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            sendMsg("Client Say: Hello!");
+        }
+
+	}
+
+    static void sendMsg(string message)
+    {
+        EndPoint point = new IPEndPoint(IPAddress.Parse(targetIP), sendPort);
+        clientUDP.SendTo(Encoding.UTF8.GetBytes(message), point);
+    }
+
+    static void ReciveMsg()
+    {
+        while (true)
+        {
+            EndPoint point = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buffer = new byte[1024];
+            int length = clientUDP.ReceiveFrom(buffer, ref point);
+            string message = Encoding.UTF8.GetString(buffer, 0, length);
+            Debug.Log(point.ToString() + message);
+        }
+    }
+}
